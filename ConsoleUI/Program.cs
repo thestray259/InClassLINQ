d7c02298c1c240a3864040e81dae29e113e5c576
Tobie Allen <tallen@student.neumont.edu>
2022-02-08 17:49:09 -0700

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqLibrary;
+using System.Linq; 
+
+namespace ConsoleUI
+{
+    public static class Extensions
+    {
+        public static void Print<T>(this IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item); 
+            }
+        }
+
+        public static void PrintPeople(this IEnumerable<Person> peopleList)
+        {
+            foreach (var person in peopleList)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}" +
+                    $"({person.Birthday.ToShortDateString()}): " +
+                    $"Experience: {person.YearsExperience} years");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ = Language Integrated Query
+            var numberList = ListManager.LoadIntSampleData();
+            List<Person> peopleList = ListManager.LoadSampleData();
+            //GetEvens(numberList); 
+            //ListPeopleName(peopleList); 
+            //GetMaxNumber(numberList); 
+            OrderByTest(peopleList); 
+        }
+
+        private static void ListPeopleName(List<Person> personList)
+        {
+            var result = personList.Select(p => p.FullName).ToList();
+            result.Print(); 
+        }
+
+        private static void OrderByTest(List<Person> personList)
+        {
+            personList = personList.OrderBy(person => person.LastName).ToList();
+            personList.PrintPeople(); 
+        }
+
+        private static void GetMaxNumber(List<int> items)
+        {
+            int largestNumber = items.Max();
+            Console.WriteLine($"Largest Number: {largestNumber}"); 
+        }
+
+        private static void GetEvens(List<int> numberList)
+        {
+            var evenNumbers = new List<int>(); 
+/*            foreach (var item in numberList)
+            {
+                if (item % 2 == 0) evenNumbers.Add(item); 
+            }
+*/
+            // Query Syntax - less common 
+/*            evenNumbers = (from number in numberList
+                           where number % 2 == 0
+                           select number).ToList();*/
+
+            // Extention Syntax
+            evenNumbers = numberList.Where(number => number % 2 == 0).ToList(); 
+
+            evenNumbers.Print(); 
+        }
+    }
+}
---
+++ LinqLibrary/ListManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqLibrary
+{
+    public class ListManager
+    {
+        public static List<Person> LoadSampleData()
+        {
+            List<Person> output = new List<Person>()
+            {
+
+                new Person{ FirstName = "Brett", LastName = "Beardall", Birthday = Convert.ToDateTime("11/01/1990"), YearsExperience = 15 },
+                new Person{ FirstName = "Holly", LastName = "Beardall", Birthday = Convert.ToDateTime("11/01/1990"), YearsExperience = 5 },
+                new Person{ FirstName = "Bob", LastName = "Builder", Birthday = Convert.ToDateTime("4/05/2009"), YearsExperience = 2 },
+                new Person{ FirstName = "Christy", LastName = "Friar", Birthday = Convert.ToDateTime("02/14/2000"), YearsExperience = 20 },
+                new Person{ FirstName = "Rick", LastName = "Astley", Birthday = Convert.ToDateTime("10/05/1965"), YearsExperience = 30 },
+                new Person{ FirstName = "Ryan", LastName = "Unroe", Birthday = Convert.ToDateTime("01/01/2000"), YearsExperience = 1 }
+            };
+
+            return output;
+        }
+
+        public static List<int> LoadIntSampleData()
+        {
+            return new List<int>() { 15, 123, 78, -49, 55, 79, 2004, 23, -344, 500, 79, 220, 0 };
+        }
+    }
+}
